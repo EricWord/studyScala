6ab49429a39e6da1080946dd77c43c018a289185
“cuigs” <cuiguangsong@miliantech.com>
2020-09-18 19:12:30 +0800

    文档注释

---
+++ src/net/codeshow/t1/Comment.scala
@@ -0,0 +1,21 @@
+package net.codeshow.t1
+
+//文档注释
+object Comment {
+  def main(args: Array[String]): Unit = {
+    println("hello,world")
+  }
+
+  /**
+   *
+   * @param n1
+   * @param n2
+   * @return 和
+   */
+  def sum(n1: Int, n2: Int): Int = {
+    return n1 + n2
+
+
+  }
+
+}
---
+++ src/net/codeshow/t1/LookSourceCode.scala
@@ -0,0 +1,11 @@
+package net.codeshow.t1
+
+object LookSourceCode {
+  def main(args: Array[String]): Unit = {
+    var arr = new Array[Int](10)
+    for (elem <- arr) {
+      println(elem)
+    }
+  }
+
+}
