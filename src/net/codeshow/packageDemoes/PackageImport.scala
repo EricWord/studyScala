102d6c38e4c3cdd78e6c44de5c9a7f4ecc3333ee
“cuigs” <cuiguangsong@miliantech.com>
2020-11-13 14:56:10 +0800

    包的使用

---
+++ src/net/codeshow/packageDemoes/PackageImport.scala
@@ -0,0 +1,11 @@
+package net.codeshow.packageDemoes
+
+
+//在scala中 java.lang包 scala包，Predef包
+object PackageImport {
+  def main(args: Array[String]): Unit = {
+    import scala.io.StdIn
+
+  }
+
+}
