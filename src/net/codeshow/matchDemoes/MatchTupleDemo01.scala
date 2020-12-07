e3f08459f85f69fb08e3e83727e899296b7c48fb
“cuigs” <cuiguangsong@miliantech.com>
2020-12-07 10:23:44 +0800

    匹配元组

---
+++ src/net/codeshow/matchDemoes/MatchTupleDemo01.scala
@@ -0,0 +1,16 @@
+package net.codeshow.matchDemoes
+
+object MatchTupleDemo01 {
+  def main(args: Array[String]): Unit = {
+
+    for (pair <- Array((0, 1), (1, 0), (10, 30), (1, 1), (1, 0, 2))) {
+      val result = pair match {
+        case (0, _) => "0 ..."
+        case (y, 0) => y
+        case (x, y) => (y, x)
+        case _ => "other"
+      }
+      println(result)
+    }
+  }
+}
